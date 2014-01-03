111f3577d19605d7741b7a887dd472c2d13e0e55
nealkhosla <nealk@cs.stanford.edu>
2014-01-03 10:43:06 -0800

    added icons for social media and did some other small polishing

--- app/static/js/class_select.js
+++ app/static/js/class_select.js
@@ -1,43 +1,9 @@
-function showSubject() {
-	var selopt = document.getElementById("subject").value;
-	var elements = document.getElementsByClassName("class_menu");
-	for (var i = 0; i < elements.length; i++) {
-		var element = elements[i]; 
-		if (element.id == selopt) {
-			element.style.display = "block";
-		}
-		else {
-			element.style.display = "none";
-		}
-	}		
-}
-
-function parseCS() {
-	var selopt = document.getElementById("cs_opts").value;
-	showClass(selopt);
-}
-
-function parseMath() {
-	var selopt = document.getElementById("math_opts").value;
-	showClass(selopt);
-}
-
-function parseMSE() {
-	var selopt = document.getElementById("mse_opts").value;
-	showClass(selopt);
-}
-
-function parseStats() {
-	var selopt = document.getElementById("stats_opts").value;
-	showClass(selopt);
-}
-
 function showClass(class_id) {
-	var selopt = document.getElementById(class_id);
+	var elem_id = class_id + "_info";
 	var elements = document.getElementsByClassName("course");
 	for (var i = 0; i < elements.length; i++) {
 		var element = elements[i];
-		if (element.id == selopt.id){
+		if (element.id == elem_id){
 			element.style.display = "block";
 		}
 		else {
@@ -45,3 +11,8 @@ function showClass(class_id) {
 		}
 	}
 } 
+
+
+
+
+
